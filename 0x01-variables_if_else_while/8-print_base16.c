ba2e8b58580669694215734b6b6ed70ee15ade11
Harrysconnect <maduuharry@gmail.com>
2023-02-20 16:57:39 -0800

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,6 +6,7 @@
  */
 
 int main(void)
+
 {
 	int num;
 	char la;
@@ -16,7 +17,7 @@ int main(void)
 	for (la = 'a'; la <= 'f'; la++)
 	putchar(la);
 
-	putchar('\n')
+	putchar('\n');
 
 			return (0);
 }
